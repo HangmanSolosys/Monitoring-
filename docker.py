df08138a1855c1f347f98687db970f2068470bd9
HangmanSoloSys <ivanignatov9624@gmail.com>
2024-11-19 22:31:50 +0000

    Adding the python script for docker

--- docker.py
+++ docker.py
@@ -1,21 +1,40 @@
 #!/bin/python3
-
-import time
-import subprocess
 import os
+import subprocess
+import time
+
+var1 = "waaazzaaap"
+
+def my_function():
+    try:
+        print("Checking if Docker is installed")
+        subprocess.run(["docker", "--version"], check=True, text=True)
+        print("Docker is installed.\n")
+
+    except FileNotFoundError:
+        print("Docker is not installed. Please install Docker first.")
+
+    except subprocess.CalledProcessError:  
+        print("Docker is installed but not functioning correctly.")
+        return
+
+    try:
+        print("Checking if Docker service is running...")
+
+        result = subprocess.run(["sudo", "systemctl", "is-active", "docker"], check=True, text=True, capture_output=True)
+        
+        if result.stdout.strip() == "active":
+            print("Docker service is running.")
+        else:
+            print("Docker service is not running.")
+
+    except subprocess.CalledProcessError:
+        print("Docker service is not running or there's an issue with the service.")
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 
-def execute_docker():
+if __name__ == "__main__":
+    my_function()
 
-    compose_file_dir="/home/solosys/containers"
-
-    os.chdir(compose_file_dir)
-
-    os.system("docker-compose up -d")
-
-    time.sleep(2)
-
-    print("The images have been pulled and the containers are started...")
-
-
-execute_docker()
